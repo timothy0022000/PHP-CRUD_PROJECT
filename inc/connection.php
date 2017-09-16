edd9bcc0fc9a0b63f945ca4b5eeb5b1e0f43b243
T.Johnson <timothy0022000@yahoo.com>
2017-09-15 22:24:20 -0500

    Add files via upload

---
+++ inc/connection.php
@@ -0,0 +1,13 @@
+<?php
+  try{
+    $db = new PDO("sqlite:".__DIR__."/database.db");
+    $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+  }catch (Exception $e){
+    
+    echo "Error !: ";
+    echo $e->getMessage();
+    
+    exit;
+  }
+  
+
